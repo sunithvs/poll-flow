dbf98ee34d1ec6c688d3a0115eba4b4c2c99df3c
sunithvs <sunithvs2002@gmail.com>
2025-01-08 19:46:51 +0530

    title change

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Poll Flow",
+  description: "The fastest and easiest way to create and share polls.",
 };
 
 const geistSans = Geist({
